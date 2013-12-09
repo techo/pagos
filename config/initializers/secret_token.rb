03cea705647ed4be72bef4cd02beef52a2726b94
Ernesto Medina <edelgado@thoughtworks.com>
2013-12-09 11:08:02 -0500

    Mercy/Ernesto/Rebecca - creating the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pagos::Application.config.secret_key_base = '25b87127872dadc59fdc2688000cad4f6fe03a8d90db1bbfa1880295465c91ce45690d744b2b1b977bdcb088903d54f687abcde131b09d46763320f0a18467f2'
