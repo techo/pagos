d1924f6bda6b5760592e07e06c64bafe2ac8fd73
Rebecca Sliter <sliterrm@gmail.com>
2013-12-11 13:59:47 -0500

    [Rebecca] #1 - added test for form helper

--- spec/helpers/form_helper_spec.rb
+++ spec/helpers/form_helper_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe FormHelper do
-  describe ".devise_error_messages!" do
-    xit "creates a list of error messages" do
-      Object.any_instance.stub(:errors).and_return(messages: {field: "this is a really bad error"})
+  include Devise::TestHelpers
 
-      devise_error_messages!.should match /<li>this is a really bad error<\/li>/
+  describe ".form_error_messages" do
+    it "creates a list of error messages" do
+      user = User.new
+      user.stub(:errors).and_return(double(messages: {field: ["this is a really bad error"]},
+                                           empty?: false))
+      helper.stub(:resource).and_return(user)
+
+      helper.form_error_messages.should match /<li>this is a really bad error<\/li>/
     end
   end
 end
