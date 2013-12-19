6f6867460dfd9ded7ba8fbda0057e425805d3a7e
Rebecca Sliter <sliterrm@gmail.com>
2013-12-19 17:28:39 -0500

    [Rebecca/Mercy/Ernesto] fixing broken tests

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -4,7 +4,7 @@ describe Payment do
 
     describe "Validations" do
       it { should validate_presence_of(:family_id).with_message("El campo FamilyId es mandatorio") }
-      it { should validate_presence_of(:amount).with_message("El campo Amount es mandatorio") }
-      it { should validate_presence_of(:date).with_message("El campo Date es mandatorio") }
+      it { should validate_presence_of(:amount).with_message("El monto es mandatorio") }
+      it { should validate_presence_of(:date).with_message("La fecha es mandatoria") }
     end
 end
