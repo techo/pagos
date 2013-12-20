2829978ae08e7ee6fdcd45e694e3ce1d2afe664b
Mercy Falconi <mfalconi@thoughtworks.com>
2013-12-20 11:28:40 -0500

    [Ernesto-Mercy] Adding valitations for payment amount

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe Payment do
@@ -6,5 +7,9 @@ describe Payment do
       it { should validate_presence_of(:family_id).with_message("El campo FamilyId es mandatorio") }
       it { should validate_presence_of(:amount).with_message("El monto es mandatorio") }
       it { should validate_presence_of(:date).with_message("La fecha es mandatoria") }
+      it { should validate_numericality_of(:amount)
+           .is_greater_than_or_equal_to(0)
+           .is_less_than_or_equal_to(9999.99)
+           .with_message("El monto debe ser numérico entre 0 y 10000") }
     end
 end
