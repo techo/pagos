7245649fbf71fad31c11df715a9e59656a9ca4f4
Mercy Falconi <mfalconi@thoughtworks.com>
2013-12-27 12:15:06 -0500

    [Mercy] Updating geographies controller for manage volunteers

--- app/controllers/geographies_controller.rb
+++ app/controllers/geographies_controller.rb
@@ -1,5 +1,12 @@
 class GeographiesController < ApplicationController
+  before_action :verify_can_manage_users
+
   def index
     @geographies = PiloteHelper.get_geographies
+    @volunteers = User.volunteers
+  end
+
+  def verify_can_manage_users
+    redirect_to root_url unless current_user && current_user.can_manage_users?
   end
 end
--- spec/controllers/geographies_controller_spec.rb
+++ spec/controllers/geographies_controller_spec.rb
@@ -1,13 +1,51 @@
 require 'spec_helper'
 
 describe GeographiesController do
+
+  describe "before filter" do
+    it "redirects to root url if the current user cannot manage users" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in FactoryGirl.create(:volunteer_user)
+
+      get :index
+      response.should redirect_to root_url
+    end
+
+    it "redirects if there is no current user" do
+      get :index
+      response.should redirect_to root_url
+    end
+
+    it "does not redirect to root url if the current user can manage users" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in FactoryGirl.create(:administrator_user)
+
+      get :index
+      response.should_not be_redirect
+    end
+  end
+
   describe "GET index" do
     it "should assign the geographies from the Pilote API" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in FactoryGirl.create(:administrator_user)
+
       pilote_response = {idAsentamiento:"1"}
       PiloteHelper.stub(:get_geographies).and_return(pilote_response)
 
       get :index
       assigns(:geographies).should == pilote_response
+    end
+
+    it "should assign the volunteers from Users" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in FactoryGirl.create(:administrator_user)
+
+      volunteer = {first_name:"juan", role:"volunteer"}
+      User.stub(:volunteers).and_return([volunteer])
+
+      get :index
+      assigns(:volunteers).should == [volunteer]
     end
   end
 end
