c40029dd4a57bd366d0aa4b3ac1055813a310a6a
Mercy Falconi <mfalconi@thoughtworks.com>
2014-01-06 15:39:41 -0500

    [Mercy] viewing debt amount in page of payments's register

--- app/models/payments_manager.rb
+++ app/models/payments_manager.rb
@@ -1,11 +1,25 @@
 class PaymentsManager
-  def self.calculating_debt ( charge, pilote_payments )
-    charge = charge.to_f
-    pilote_payments = pilote_payments.to_f
-    if ( charge > pilote_payments )
-      charge - pilote_payments
+
+  def self.calculating_debt ( family )
+    family_id = family["id_de_familia"].to_i
+    charge = family["monto_original"].to_f
+    pilote_payments = family["pagos"].to_f
+    
+    localPayments = Payment.where(:family_id => family_id)
+    currentPayments = 0
+
+    if ( localPayments.count > 0 )
+      currentPayments = localPayments.sum(:amount)
+    else
+      Payment.create(family_id:family_id, amount:pilote_payments, date: DateTime.now )
+      currentPayments = pilote_payments
+    end
+
+    if ( charge >= currentPayments )
+      charge - currentPayments
     else
       -1
     end
   end
+
 end
--- spec/models/payments_manager_spec.rb
+++ spec/models/payments_manager_spec.rb
@@ -3,17 +3,47 @@ require "spec_helper"
 describe PaymentsManager do
   describe "calculating_debt" do
     it "should return current debt for the specified parameters" do
-      charge = "1000"
-      pilote_payments = "100"
-      debt = PaymentsManager.calculating_debt charge, pilote_payments
-      debt.should == 900
+      family = {"id_de_familia" => "1", "monto_original" => "200", "pagos" => "50"}
+      debt = PaymentsManager.calculating_debt family
+      debt.should == 150
     end
 
     it "should return -1 if charge is lesser than payments" do
-      charge = "100"
-      pilote_payments = '1000'
-      debt = PaymentsManager.calculating_debt charge, pilote_payments
+      family = {"id_de_familia" => "1", "monto_original" => "100", "pagos" => "1000"}
+      debt = PaymentsManager.calculating_debt family
       debt.should == -1
+    end
+
+    it "should save a first payment if it is not registered locally" do
+      family = {"id_de_familia" => "1", "monto_original" => "200", "pagos" => "50"}
+      @join = double(ActiveRecord::Relation)
+      @join.stub(:count).and_return(0)
+      Payment.should_receive(:where).with(:family_id => 1).and_return(@join)
+      expect {
+        debt = PaymentsManager.calculating_debt family
+      }.to change{Payment.count }.by(1)
+    end
+
+    it "should not save any payment if it is registered locally" do
+      family = {"id_de_familia" => "1", "monto_original" => "200", "pagos" => "50"}
+      @join = double(ActiveRecord::Relation)
+      @join.stub(:count).and_return(2)
+      @join.should_receive(:sum).with(:amount).and_return(100)
+      Payment.should_receive(:where).with(:family_id => 1).and_return(@join)
+      expect {
+        debt = PaymentsManager.calculating_debt family
+      }.to change{Payment.count }.by(0)
+    end
+
+    it "should take off from the debt the local payments" do
+      family = {"id_de_familia" => "1", "monto_original" => "200", "pagos" => "50"}
+      @join = double(ActiveRecord::Relation)
+      @join.stub(:count).and_return(2)
+      @join.should_receive(:sum).with(:amount).and_return(150)
+      Payment.stub(:where).with(:family_id => 1).and_return(@join)
+
+      debt = PaymentsManager.calculating_debt family
+      debt.should == 50
     end
   end
 end
