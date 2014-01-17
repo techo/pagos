80a3d517f10b074c87821f6d9d46aa67940a6bf7
Rebecca Sliter <sliterrm@gmail.com>
2014-01-17 11:17:10 -0500

    [Rebecca] - changing first admin to Guillermo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Create initial admin user
 if (User.all.empty?)
-  u = User.new(first_name: "Michelle", last_name: "Arevalo-Carpenter", email:"michelle.arevalo@techo.org", password: ENV["ADMIN_DEFAULT_PASSWORD"], role:"administrador")
+  u = User.new(first_name: "Guillermo", last_name: "Lincango", email:"guillermo.lincango@techo.org", password: ENV["ADMIN_DEFAULT_PASSWORD"], role:"administrador")
   u.save(validation: false)
 end
