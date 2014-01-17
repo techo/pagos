3c98cbd572ece9d07e0f63b5f7264201bb068560
Rebecca Sliter <sliterrm@gmail.com>
2014-01-17 11:26:23 -0500

    [Rebecca] - tiny change to trigger deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Create initial admin user
+# Create initial admin user-- guille
 if (User.all.empty?)
   u = User.new(first_name: "Guillermo", last_name: "Lincango", email:"guillermo.lincango@techo.org", password: ENV["ADMIN_DEFAULT_PASSWORD"], role:"administrador")
   u.save(validation: false)
