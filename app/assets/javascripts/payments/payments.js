d2e843369159547cc91623d156d37ca8f126ddcd
Ernesto Medina <edelgado@thoughtworks.com>
2014-01-23 10:30:48 -0500

    [Batman/Ernesto] - Adding javascript code to avoid multiples form submitions

--- app/assets/javascripts/payments/payments.js
+++ app/assets/javascripts/payments/payments.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-  $(':checkbox').each(function(index,checkbox){
-    $(checkbox).on('change', function(){
-      id = $(this).attr('data-familyid');
-      $('#deposit_number_' + id ).toggle();
-      deposit = $('#deposit_number_' + id + ' input:first');
-      deposit.val('');
-    });
+  $(":checkbox").on("change", function(){
+    $(this).parent().next().toggle();
   });
 
-  $(':submit').each(function(index, button){
-      $(button).click(function(event) {
-      $(this).closest('form').valid();
-    })
-  })
+  $("form").on("submit", function(){
+    var $form = $(this);
+    if($form.valid()){
+      $form.find(".btn").attr("disabled", true).val("Enviando...");
+    }else{
+      return false;
+    }
+  });
+
 });
