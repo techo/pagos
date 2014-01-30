fb1b29a342735b95a204728e02df7fb8de1db3d7
Sri Prasanna <k.sriprasanna@gmail.com>
2014-01-30 17:18:18 -0500

    Fixing build

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,7 @@ Pagos::Application.configure do
   ENV["PILOTE_ROOT"] = "https://pilotes.ec:8888"
   ENV["PILOTE_USERNAME"] = "USERNAME"
   ENV["PILOTE_PASSWORD"] = "PASSWORD"
+  ENV["PILOTE_COUNTRY_CODE"] = "10"
 
   ENV["ADMIN_DEFAULT_PASSWORD"] = "0123456789"
 end
