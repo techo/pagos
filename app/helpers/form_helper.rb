270dddded87eb7aa9cabbbd59ec352b9313c3b36
Ernesto Medina <ernezto@gmail.com>
2014-02-03 17:15:45 -0500

    [Mercy/Ernesto] - fixing errors messages in payment helper

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -3,7 +3,7 @@ module FormHelper
     resource_with_errors = form_resource ? form_resource : resource
     return '' if resource_with_errors.errors.empty?
 
-    messages = resource_with_errors.errors.messages.map { |field, msg| content_tag(:li, msg.join(', ')) }.join
+    messages = resource_with_errors.errors.messages.map { |field, msg| content_tag(:li, msg.join(', ')) if !msg.blank? }.join
 
     html = <<-HTML
     <div class="alert alert-error alert-block">
--- spec/helpers/form_helper_spec.rb
+++ spec/helpers/form_helper_spec.rb
@@ -11,6 +11,15 @@ describe FormHelper do
       helper.form_error_messages.should match /<li>this is a really bad error<\/li>/
     end
 
+    it "should not include only one error message" do
+      user = User.new
+      user.stub(:errors).and_return(double(messages: {field: ["this is a really bad error"], field1: []},
+                                           empty?: false))
+      helper.stub(:resource).and_return(user)
+
+      helper.form_error_messages.scan("<li>").size.should == 1
+    end
+
     it "should accept a form resource as an optional parameter" do 
       user = User.new
       user.stub(:errors).and_return(double(messages: {field: ["this is a really bad error"]},
